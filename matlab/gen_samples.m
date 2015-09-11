2ecdc510a3dcdf95749a0b8b4da1971a2246ae4e
Louis Whitcomb <llw@jhu.edu>
2015-09-11 12:56:27 -0400

    revised gen_samples.m for clarity

--- matlab/gen_samples.m
+++ matlab/gen_samples.m
@@ -12,6 +12,7 @@ w_se = [0;0;15*pi/(3600*180)];
 
 % define instrument angular vel in ned frame and the x,v,a of the
 % instrument in ned frame
+
 w_ni = 0*[zeros(1,num_samples);zeros(1,num_samples);ones(1,num_samples)*(5*pi/180)];
 a_n  = 0*[zeros(1,num_samples);zeros(1,num_samples);-sin(t)];
 v_n  = 0*[zeros(1,num_samples);zeros(1,num_samples);cos(t)];
@@ -39,12 +40,14 @@ w_i_pure = cellfun(@(A,B) A'*B,R_ni,num2cell(w_ni,1),'UniformOutput',false);
 w_pure   = cellfun(@(A,B) A+B'*w_se,w_i_pure,R_si,'UniformOutput',false);
 
 % noise sigmas
-w_sig = (2/(10000*sqrt(1/hz)))*pi/180;
-a_sig = .12*sqrt(3)/(1000*sqrt(1/hz));
+w_sig = (2/(10000*sqrt(1/hz)))*pi/180;  % units are rad/sec
+a_sig = 0.12*sqrt(3)/(1000*sqrt(1/hz)); % units are g, not m/s^2
 
-% bias sigmas
-w_bias_sig = (.1/3600)*pi/180;
-a_bias_sig = .05/1000;
+% bias sigmas 
+% w_bias_sig = (.1/3600)*pi/180;
+% a_bias_sig = .05/1000;
+w_bias_sig = 0;
+a_bias_sig = 0;
 
 % generate acc component in star frame due to instrument a in ned frame --- R_sn*a_n
 a_veh  = cellfun(@(A,B) A*B,R_sn,num2cell(a_n,1),'UniformOutput',false);
@@ -67,23 +70,34 @@ a_pure = cellfun(@(A,B) A'*B,R_si,a_tot,'UniformOutput',false);
 w_noise = w_bias_sig*randn(num_samples,3) + w_sig*randn(num_samples,3);
 a_noise = a_bias_sig*randn(num_samples,3) + a_sig*randn(num_samples,3);
 
-%samp.w_noise  = w_noise;
-%samp.a_noise  = a_noise;
-% samp.w_i_pure = w_i_pure;
-% samp.w_pure   = w_pure;
-% samp.g_s      = g_s;
-% samp.a_veh    = a_veh;
-% samp.a_cor    = a_cor;
-% samp.a_cent   = a_cent;
-% samp.a_pure   = a_pure;
-% samp.a_tot    = a_tot;
-% samp.a_tot2   = a_tot2;
-% samp.R_se     = R_se;
-% samp.R_en     = R_en;
-% samp.R_ni     = R_ni;
+% store working variables in output structure
+samp.num_samples = num_samples;
+samp.g = g
+samp.r_e = r_e;
+samp.g_e = g_e;
+samp.w_se = w_se;
+
+samp.w_sig = w_sig;
+samp.a_sig = a_sig;
+
+samp.w_noise  = w_noise;
+samp.a_noise  = a_noise;
+samp.w_i_pure = w_i_pure;
+samp.w_pure   = w_pure;
+% samp.g_s    = g_s;
+samp.a_veh    = a_veh;
+samp.a_cor1   = a_cor1;
+samp.a_cor    = a_cor;
+samp.a_cent   = a_cent;
+samp.a_pure   = a_pure;
+samp.a_tot    = a_tot;
+samp.a_tot2   = a_tot2;
+samp.R_se     = R_se;
+samp.R_en     = R_en;
+samp.R_ni     = R_ni;
 samp.R_si     = R_si;
 samp.t        = t;
 
 % generate samples
 samp.ang = cell2mat(w_pure)'   + w_noise;
-samp.acc = cell2mat(a_pure)'/g + a_noise/3;
+samp.acc = cell2mat(a_pure)' *(1.0/g) + a_noise/3;
