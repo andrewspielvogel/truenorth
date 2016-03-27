d83a65f2cb5bbfc0016508c039ebf10663cab47f
Andrew Spielvogel <aspielv1@jhu.edu>
2016-03-26 22:26:28 -0400

    update plot_adap to do bias components

--- matlab/plot_adap.m
+++ matlab/plot_adap.m
@@ -1,12 +1,35 @@
 function f = plot_adap(out)
 
 figure
-subplot(2,1,1);
-plot(out.t,out.bias.acc,out.t,repmat(out.true.bias.acc,1,size(out.t,1)));
+subplot(3,1,1);
+plot(out.t,out.bias.acc(:,1),out.t,repmat(out.true.bias.acc(1),1,size(out.t,1)));
 xlabel('time [s]');
-ylabel('acc [g]');
-subplot(2,1,2);
-plot(out.t,out.bias.ang,out.t,repmat(out.true.bias.ang,1,size(out.t,1)));
+ylabel('acc_x [g]');
+
+subplot(3,1,2);
+plot(out.t,out.bias.acc(:,2),out.t,repmat(out.true.bias.acc(2),1,size(out.t,1)));
 xlabel('time [s]');
-ylabel('ang [rad/s]');
-legend('x_{est}','y_{est}','z_{est}','x_{true}','y_{true}','z_{true}');
+ylabel('acc_y [g]');
+
+subplot(3,1,3);
+plot(out.t,out.bias.acc(:,3),out.t,repmat(out.true.bias.acc(3),1,size(out.t,1)));
+xlabel('time [s]');
+ylabel('acc_z [g]');
+
+figure
+subplot(3,1,1);
+plot(out.t,out.bias.ang(:,1),out.t,repmat(out.true.bias.ang(1),1,size(out.t,1)));
+xlabel('time [s]');
+ylabel('ang_x [rad/s]');
+
+subplot(3,1,2);
+plot(out.t,out.bias.ang(:,2),out.t,repmat(out.true.bias.ang(2),1,size(out.t,1)));
+xlabel('time [s]');
+ylabel('ang_y [rad/s]');
+
+subplot(3,1,3);
+plot(out.t,out.bias.ang(:,3),out.t,repmat(out.true.bias.ang(3),1,size(out.t,1)));
+xlabel('time [s]');
+ylabel('ang_z [rad/s]');
+
+legend('est','true');
