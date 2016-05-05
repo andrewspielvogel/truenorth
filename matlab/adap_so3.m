1628bc6af9d5b66cc71147a63ffb24eb539fd0ab
aspiel <andrewspielvogel@gmail.com>
2016-05-05 12:41:01 -0400

    add gain

--- matlab/adap_so3.m
+++ matlab/adap_so3.m
@@ -5,12 +5,13 @@ R{num_samp} = eye(3);
 R{1} = eye(3);
 err = zeros(3,num_samp);
 
+k=0.5;
 
 for i=2:num_samp
     
     dt = t(i)-t(i-1);
     y_est = R{i-1}*u(:,i-1);
-    err(:,i-1) = R{i-1}'*cross(y_est,y(:,i-1));
+    err(:,i-1) = k*R{i-1}'*cross(y_est,y(:,i-1));
     R{i} = R{i-1}*expm(dt*skew(err(:,i-1)));
     
     
