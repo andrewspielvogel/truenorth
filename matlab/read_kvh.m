cee5eb35dab4911447fccd701cd31c25114eca9a
aspiel <andrewspielvogel@gmail.com>
2016-05-20 18:15:52 -0400

    add status info to log file

--- matlab/read_kvh.m
+++ matlab/read_kvh.m
@@ -15,5 +15,6 @@ out.mag = csv_input(:,7:9)';
 out.temp = csv_input(:,10)';
 out.seq_num = csv_input(:,11)';
 out.stamp = csv_input(:,12)';
+out.status = csv_input(:,13:18)';
 out.t = out.stamp;
 out.hz = round(1/(((out.stamp(end)-out.stamp(1)))/size(out.stamp,2)));
