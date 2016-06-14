eebc90ebad1447cd23785b0d0e703f9180b64881
aspiel <andrewspielvogel@gmail.com>
2016-06-14 19:45:19 +0000

    update matlab att est

--- matlab/adap_Rbar.m
+++ matlab/adap_Rbar.m
@@ -4,8 +4,6 @@ tic
 
 lat = pi/180*lat;
 
-num_samp  = size(samp.stamp,2);
-
 data = samp;
 data.acc = samp.acc';
 data.ang = samp.ang';
@@ -17,61 +15,45 @@ Rd   = get_R_d(data,eye(3));
 disp('Computing Rb');
 
 
-u_a = zeros(3,num_samp);
-y_a = zeros(3,num_samp);
-y_w = zeros(3,num_samp);
-u_w = samp.ang;
-
-Rsn{num_samp} = zeros(3,3);
-
-for i=1:num_samp
-    
-    
-    Rsn{i} = get_Rsn(lat,samp.stamp(i));
-    y_a(:,i) = Rsn{i}*[0;0;-1];
-    u_a(:,i) = Rd{i}*samp.acc(:,i);
-    y_w(:,i) = [-sin(lat),0,cos(lat)];
-    %u_w(:,i) = samp.ang(:,i);
-    
-end
-
 R0 = [-sin(lat),0,-cos(lat);0,1,0;cos(lat),0,-sin(lat)]*R_align;
-wrand = [3,10,10];
+wrand = [10,10,10];
 
 R_distr = rph2R(wrand*pi/180);
-R0 = eye(3); %R0*R_distr;
-Rb = adap_so3(y_a,u_a,y_w,u_w,samp.stamp,R0,Rd);
+R0 = R0*R_distr;
+Rb = adap_so3(lat,samp,R0,Rd);
 
 disp('Computing att');
 
-out.u_a = u_a;
-out.y_a = y_a;
-out.y_w = y_w;
-out.u_w = u_w;
+
 out.Rb = Rb;
-out.Rsn = Rsn;
 out.Rd = Rd;
+out.Rsn = get_Rsn(lat,samp.stamp);
 out.Rsi = cellfun(@(A,B) A*B,Rb.R,Rd,'UniformOutput',false);
-out.Rin = cellfun(@(A,B) R_align*B'*A,Rsn,out.Rsi,'UniformOutput',false);
+out.Rin = cellfun(@(A,B) R_align*B'*A,out.Rsn,out.Rsi,'UniformOutput',false);
 out.att = rph(out.Rin);
 out.t = samp.stamp;
 
 toc 
 
 
-function R = get_Rse(t)
+
+function Rse = get_Rse(t)
 
 rate = 15*pi/180/3600;
 
-Rse = expm(skew([0,0,1])*rate*t);
+num = size(t,2);
+Rse{num} = eye(3);
 
-R = Rse;
+for i = 1:num
+   
+    Rse{i} = expm(skew([0,0,1])*rate*t(i));
+
+end
+
 
 function R = get_Rsn(lat,t)
 
 Ren = [-sin(lat),0,-cos(lat);0,1,0;cos(lat),0,-sin(lat)];
 Rse = get_Rse(t);
 
-R = Rse*Ren;
-
-
+R = cellfun(@(A) A*Ren,Rse,'UniformOutput',false);
--- matlab/adap_so3.m
+++ matlab/adap_so3.m
@@ -1,53 +1,69 @@
-function out = adap_so3(y_a,u_a,y_w,u_w,t,R0,Rd)
+function out = adap_so3(lat,samp,R0,Rd)
 
-num_samp = size(u_a,2);
+num_samp = size(samp.acc,2);
 R{num_samp} = eye(3);
 R{1} = R0;
-err_a = zeros(3,num_samp);
-err_w = zeros(3,num_samp);
-y_est_a = zeros(3,num_samp);
-cor_w = zeros(3,num_samp);
-w_s = zeros(3,num_samp);
-w_a = zeros(3,num_samp);
-a_est = zeros(3,num_samp);
+
+a_true  = zeros(3,num_samp);
+a_est   = zeros(3,num_samp);
+a_error = zeros(3,num_samp);
+
+e_true  = zeros(3,num_samp);
+e_est   = zeros(3,num_samp);
+e_error = zeros(3,num_samp);
+
 
 k_a = 1;
-k_w1 = 500;
-k_w2 = 50;
+k_w = 500;
+k_w2 = 30;
 
 for i=2:num_samp
     
-    dt = t(i)-t(i-1);
-    y_est_a(:,i-1) = R{i-1}*u_a(:,i-1);
-    err_a(:,i-1) = k_a*R{i-1}'*cross(y_est_a(:,i-1),y_a(:,i-1));
-    
-    if num_samp > 60*5000*5
+    if samp.stamp(i)> 60*3
         k_w = k_w2;
-    else
-        k_w = k_w1;
     end
     
+    dt = samp.stamp(i) - samp.stamp(i-1);
     
-    a_est(:,i-1) = Rd{i-1}'*R{i-1}'*y_a(:,i-1);
+    a_true(:,i-1) = get_Rsn(lat,samp.t(i-1))*[0;0;-1];
+    a_est(:,i-1)  = R{i-1}*Rd{i-1}*samp.acc(:,i-1);
+    
+    a_error(:,i-1) = k_a*R{i-1}'*cross(a_est(:,i-1),a_true(:,i-1));
     
     
-    w_a(:,i-1) = -skew(a_est(:,i-1))*skew(a_est(:,i-1))*u_w(:,i-1);
+    e_true(:,i-1) = get_Rsn(lat,samp.t(i-1))*[0;1;0];
+    e_est(:,i-1)  = R{i-1}*Rd{i-1}*skew(samp.ang(:,i-1))*samp.acc(:,i-1);
     
-    w_s(:,i-1) = R{i-1}*Rd{i-1}*w_a(:,i-1);
+    e_error(:,i-1) = k_w*R{i-1}'*cross(e_est(:,i-1),e_true(:,i-1));
     
-    err_w(:,i-1) = cross(w_s(:,i-1),y_w(:,i-1));
     
-    cor_w(:,i-1) = k_w*R{i-1}'*err_w(:,i-1);
+    dR = expm(skew(a_error(:,i-1)+e_error(:,i-1))*dt);
     
-    R{i} = R{i-1}*expm(dt*skew(err_a(:,i-1)+cor_w(:,i-1)));
+    R{i} = R{i-1}*dR;
     
     
 end
+
 out.R = R;
-out.y_est_a = y_est_a;
-out.err_a = err_a;
-out.cor_w = cor_w;
-out.err_w = err_w;
-out.w_s = w_s;
-out.w_a = w_a;
-out.a_est = a_est;
+out.a_true  = a_true;
+out.a_est   = a_est;
+out.a_error = a_error;
+out.e_true  = e_true;
+out.e_est   = e_est;
+out.e_error = e_error;
+
+
+function R = get_Rse(t)
+
+rate = 15*pi/180/3600;
+
+Rse = expm(skew([0,0,1])*rate*t);
+
+R = Rse;
+
+function R = get_Rsn(lat,t)
+
+Ren = [-sin(lat),0,-cos(lat);0,1,0;cos(lat),0,-sin(lat)];
+Rse = get_Rse(t);
+
+R = Rse*Ren;
