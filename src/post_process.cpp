f8df7d10ff5ff13ded4121cb2e383922ceb0b219
aspiel <andrewspielvogel@gmail.com>
2016-07-14 22:38:16 +0000

    made post_process node

--- src/post_process.cpp
+++ src/post_process.cpp
@@ -1,19 +1,59 @@
 #include <stdlib.h>
 #include <iostream>
+#include <Eigen/Core>
 #include <Eigen/Dense>
 #include <truenorth/helper_funcs.h>
 #include <truenorth/att_est.h>
 #include <string>
+#include <math.h>
 
 int main(int argc, char* argv[])
 {
 
+  int rows = 5000*60*7;
+  int cols = 21;
+  float lat = 39.32*180/M_PI;
+  Eigen::Matrix3d R_align;
+  R_align << 1,0,0,0,-1,0,0,0,-1;
+  Eigen::VectorXd k(4);
+  k << 1,.001,1,.01; //a,e,g,w
+
+  std::string name_out = "test.csv";
+
   std::string file = "/home/spiels/log/KVH/static_run3/2016_7_13_15_23.KVH";
 
-  Eigen::MatrixXd data = readCSV(file,5,21);
+  printf("LOADING CSV FILE: %s\n",file.c_str());
 
-  Eigen::VectorXd k(4);
-  k << 1,.0001,1,.005; //a,e,g,q
-  AttEst att(k,Eigen::Matrix3d::Identity(3,3));
+  Eigen::MatrixXd data = readCSV(file,rows,cols);
+
+  printf("CSV FILE LOADED: %s\n",file.c_str());
+
+  Eigen::Matrix3d R0;
+  R0 = get_R_sn(lat,0)*R_align;
+
+  AttEst att(k,R0);
+
+  Eigen::MatrixXd trph(4,rows-1);
+
+  for (int i=1; i<rows; i++) {
+
+    att.step(data.block<1,3>(i,0).transpose(),data.block<1,3>(i,3).transpose(),data(i,11)-data(0,11),data(i,11)-data(i-1,11));
+   
+    trph(0,i-1) = data(i,11);
+    trph.block<3,1>(1,i-1) = rot2rph(att.R_ni*R_align);
+
+    if ((i+1) % (5000*60) == 0){
+
+      std::cout<<trph.block<3,1>(0,i-1)*180/M_PI<<std::endl<<std::endl;
+
+    }
+
+  }
+
+  Eigen::IOFormat CSVFormat(Eigen::FullPrecision, Eigen::DontAlignCols, ", ", "\n");
+  std::ofstream ofile(name_out.c_str());
+  ofile << trph.format(CSVFormat);
+  ofile.close();
+  
 
 }
