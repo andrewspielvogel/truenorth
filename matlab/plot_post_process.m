418a136b9320ded4955411fe0fd94819fd06114c
aspiel <andrewspielvogel@gmail.com>
2016-07-20 21:36:19 +0000

    update plot_post_process for updated att_est signals

--- matlab/plot_post_process.m
+++ matlab/plot_post_process.m
@@ -13,6 +13,12 @@ out.t = data_parsed(1,:);
 out.stamp = out.t;
 out.att = data_parsed(2:4,:)';
 out.Rb_rph = data_parsed(5:7,:);
+out.acc_est = data_parsed(8:10,:);
+out.east_est_z = data_parsed(11:13,:);
+out.east_error = data_parsed(14:16,:);
+out.g_error = data_parsed(17:19,:);
+out.ang_est = data_parsed(20:22,:);
+out.dacc = data_parsed(23:25,:);
 
 figure;
 plot(data_parsed(1,:),data_parsed(2:4,:)*180/pi);
