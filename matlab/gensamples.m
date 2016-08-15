a3d815549a24dd4ea5312f92de41f7a78d4b9bb3
aspiel <andrewspielvogel@gmail.com>
2016-08-15 18:24:51 +0000

    update sample generation

--- matlab/gensamples.m
+++ matlab/gensamples.m
@@ -11,8 +11,6 @@ if nargin<5
     
 end
 
-Rb = [-sin(lat),0,-cos(lat);0,1,0;cos(lat),0,-sin(lat)]*R_align;
-
 
 % Define t
 dt = 1/hz;
@@ -31,38 +29,48 @@ samp.ang = zeros(3,num);
 samp.acc = zeros(3,num);
 samp.att = zeros(3,num);
 
-samp.Rd{1} = eye(3);
+samp.Rni{1} = eye(3);
 
+Ren = [-sin(lat),0,-cos(lat);0,1,0;cos(lat),0,-sin(lat)];
+a_e = [cos(lat);0;sin(lat)] - (15*pi/180/3600)^2*cos(lat)*[r;0;0]/9.81;
+a_e = a_e/norm(a_e);
+a_n = Ren'*a_e;
+
+Rsz = get_Rsn(lat,0)*R_align;
+samp.Rsz=Rsz;
+w_earth_z = Rsz'*[0;0;1]*15*pi/180/3600;
+    
 for i=1:num
 
-    a_e = [cos(lat);0;sin(lat)] - (15*pi/180/3600)^2*cos(lat)*[r;0;0];
-    a_e = a_e/norm(a_e);
+
     
-    w = get_w(t(i));
-    
+    w_veh = get_w(t(i));
     Rsn = get_Rsn(lat,t(i));
     
-    
     if i~=num
-        samp.Rd{i + 1} = samp.Rd{i}*expm(skew(w)*dt);
+        samp.Rni{i + 1} = samp.Rni{i}*expm(skew(w_veh)*dt);
     end
-    samp.Rsi{i} = Rsn*samp.Rd{i};
+    samp.Rsi{i} = Rsn*samp.Rni{i};
+    samp.Rd{i} = Rsz'*Rsn*samp.Rni{i};
+    samp.Rsn{i} = Rsn;
+
+    samp.att(:,i) = rot2rph(samp.Rni{i});  
     
-    samp.ang(:,i) = R_align*(samp.Rsi{i}'*Rsn*w + samp.Rsi{i}'*[0;0;1]*15*pi/180/3600 + w_sig*randn(3,1) + bias.ang);
-    samp.acc(:,i) = R_align*(samp.Rsi{i}'*get_Rse(t(i))*a_e  + a_sig*randn(3,1) + bias.acc);
-    
-    samp.att(:,i) = rot2rph(Rsn'*samp.Rsi{i});
-    
+    samp.ang(:,i) =  samp.Rni{i}'*w_veh + samp.Rsi{i}'*Rsz*w_earth_z + w_sig*randn(3,1) + bias.ang;
+    samp.acc(:,i) =  samp.Rni{i}'*a_n + a_sig*randn(3,1) + bias.acc;
+    samp.acc_z(:,i) = samp.Rd{i}*samp.acc(:,i);
+   
+        samp.w_v(:,i) = w_veh;
+samp.e_v(:,i) = skew(w_veh)*samp.acc(:,i);
     % print progress
     if ~mod(t(i),30)
         str = sprintf('Made %i:%i0 of data at %i hz',floor(t(i)/60),mod(t(i),60)/10,hz);
         disp(str);
     end
-    
+        
 end
 
 % save
-samp.Rb = Rb;
 samp.t = t;
 samp.stamp = t;
 samp.hz = hz;
@@ -85,4 +93,15 @@ Ren = [-sin(lat),0,-cos(lat);0,1,0;cos(lat),0,-sin(lat)];
 Rse = get_Rse(t);
 
 R = Rse*Ren;
+
+function w = get_w(t)
+
+if t<5*60*0
+    w=[0;0;0];
+else
+
+w = [cos(t)/7;sin(t*2.3)/4;0];
+w = [cos(t/2)/20;sin(t)/10;cos(t/5)/40];
+end
+
 
--- matlab/get_w.m
+++
@@ -1,3 +0,0 @@
-function w = get_w(t)
-
-w = [0;0;sin(t/20)/20];
