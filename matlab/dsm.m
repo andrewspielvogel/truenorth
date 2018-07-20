561d765850282b8f3cc6e76bc2e90eb151f0a89e
Andrew Spielvogel <aspielv1@jhu.edu>
2018-07-20 11:15:11 -0400

    break matlab code into own repo

--- matlab/dsm.m
+++
@@ -1,4 +0,0 @@
-function ans = dsm(unix_time)
-% Mar 23 2007 LLW Computes monotinic days since midnight
-
-ans = ssm_noclash(unix_time) * (1/(24*3600));
--- matlab/r_lat_long.m
+++
@@ -1,3 +0,0 @@
-function r = r_lat_long(lat)
-
-r = [cos(lat);0;sin(lat)];
