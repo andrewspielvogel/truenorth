91a74ae0007b85bf8cea74604da6d92e4a13ef37
data <data@jhu.edu>
2018-08-22 19:48:18 +0000

    fixed file path for backup-backup script to whole dives directory

--- scripts/sync_backup_hard_drives.sh
+++ scripts/sync_backup_hard_drives.sh
@@ -2,12 +2,12 @@
 # 2018-08-22 RH Created for Truenorth data syncing
 
 # Destination dir of the data
-DEST_DIR='/media/data/Sentry08-Data2/2018-sentry-gyro/dives/test-dive'
+DEST_DIR='/media/data/Sentry08-Data2/2018-sentry-gyro/dives/'
 
 # Source of the data. This will be the sentry kvh stack
-SRC_DIR='/media/data/Sentry08-Data1/2018-sentry-gyro/dives/test-dive'
+SRC_DIR='/media/data/Sentry08-Data1/2018-sentry-gyro/dives/'
 
-RSYNC_CMD="rsync -avh $SRC_DIR $DEST_DIR"
+RSYNC_CMD="rsync -avh --progress $SRC_DIR $DEST_DIR"
 echo $RSYNC_CMD 
 $RSYNC_CMD
 
