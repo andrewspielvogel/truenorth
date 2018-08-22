07217d6fe97b325cdec9c039e61f36213ff605ea
data <data@jhu.edu>
2018-08-22 19:22:25 +0000

    tested rsync script

--- scripts/backup_data_to_sea_server.sh
+++ scripts/backup_data_to_sea_server.sh
@@ -2,14 +2,15 @@
 # 2018-08-22 RH Created for Truenorth data syncing
 
 # Destination dir of the data
-DEST_DIR='/media/data/Sentry08-Data1/2018-sentry-gyro/dives/test-dive'
+DEST_DIR='/media/data/Sentry08-Data1/2018-sentry-gyro/dives/test-dive-2/'
 
 # Source of the data. This will be the sentry kvh stack
 SRC_IP='192.168.100.112'
 SRC_USER='spiels'
 SRC_DIR='/log/'
 
-RSYNC_CMD="rsync -avh $SRC_USER@$SRC_IP:$SRC_DIR $DEST_DIR"
+RSYNC_CMD="rsync -avh --progress $SRC_USER@$SRC_IP:$SRC_DIR $DEST_DIR"
 echo $RSYNC_CMD 
+
 $RSYNC_CMD
 
