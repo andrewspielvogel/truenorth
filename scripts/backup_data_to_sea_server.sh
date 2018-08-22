e3afb5cd5e90faa5423b4ff7207eb855ea69a8c6
Rachel <rachel.hegeman@gmail.com>
2018-08-22 16:02:08 -0300

    added data backup script

---
+++ scripts/backup_data_to_sea_server.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# 2018-08-22 RH Created for Truenorth data syncing
+
+# Destination dir of the data
+DEST_DIR='/media/data/Sentry08-Data1/2018-sentry-gyro/dives/test-dive'
+
+# Source of the data. This will be the sentry kvh stack
+SRC_IP='192.168.100.112'
+SRC_USER='spiels'
+SRC_DIR='/log/'
+
+RSYNC_CMD="rsync -avh $SRC_USER@$SRC_IP:$SRC_DIR $DEST_DIR"
+echo $RSYNC_CMD 
+$RSYNC_CMD
+
