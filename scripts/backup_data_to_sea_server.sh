6b3d0115c049d801c10380048e70363c587bb732
Andrew Spielvogel <andrewspielvogel@gmail.com>
2018-08-23 14:04:32 +0000

    update backup_data_to_sea_server

--- scripts/backup_data_to_sea_server.sh
+++ scripts/backup_data_to_sea_server.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 # 2018-08-22 RH Created for Truenorth data syncing
 
+###########################################
+###########################################
+#   RUN FROM DATA USER ON GIT SERVER COMPUTER (192.168.100.212)
+###########################################
+###########################################
+
+
+
+
+
+
+
 # Destination dir of the data
-DEST_DIR='/media/data/Sentry08-Data1/2018-sentry-gyro/dives/test-dive-2/'
+DEST_DIR='/media/data/Sentry08-Data1/2018-sentry-gyro/dives/sentry490/'
 
 # Source of the data. This will be the sentry kvh stack
 SRC_IP='192.168.100.112'
 SRC_USER='spiels'
 SRC_DIR='/log/'
 
-RSYNC_CMD="rsync -avh --progress $SRC_USER@$SRC_IP:$SRC_DIR $DEST_DIR"
+RSYNC_CMD="rsync -avh --dry-run --progress $SRC_USER@$SRC_IP:$SRC_DIR $DEST_DIR"
 echo $RSYNC_CMD 
 
 $RSYNC_CMD
