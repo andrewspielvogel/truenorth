26bf84b6a58a63d06bcb62bb9e4b949ad899c140
data <data@jhu.edu>
2018-08-25 11:42:43 +0000

    set up backup script for next dive: sentry493

--- scripts/backup_data_to_sea_server.sh
+++ scripts/backup_data_to_sea_server.sh
@@ -14,7 +14,7 @@
 
 
 # Destination dir of the data
-DEST_DIR='/media/data/Sentry08-Data1/2018-sentry-gyro/dives/sentry490/'
+DEST_DIR='/media/data/Sentry08-Data1/2018-sentry-gyro/dives/sentry493/'
 
 # Source of the data. This will be the sentry kvh stack
 SRC_IP='192.168.100.112'
@@ -28,7 +28,6 @@ SRC_DIR='/log/'
 RSYNC_CMD="rsync -avh --dry-run --progress $SRC_USER@$SRC_IP:$SRC_DIR $DEST_DIR"
 # THE REAL THING
 # RSYNC_CMD="rsync -avh --progress $SRC_USER@$SRC_IP:$SRC_DIR $DEST_DIR"
-
 
 echo $RSYNC_CMD 
 
